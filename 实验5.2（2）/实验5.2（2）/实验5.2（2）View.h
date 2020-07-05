ced829ad11bab7802a8f75e07546a9daaac75705
levi-Ackerman6 <2535133278@qq.com>
2020-07-06 02:20:35 +0800

    522

---
+++ 实验5.2（2）/实验5.2（2）/实验5.2（2）View.h
@@ -0,0 +1,48 @@
+
+// 实验5.2（2）View.h : C实验52（2）View 类的接口
+//
+
+#pragma once
+
+
+class C实验52（2）View : public CView
+{
+protected: // 仅从序列化创建
+	C实验52（2）View();
+	DECLARE_DYNCREATE(C实验52（2）View)
+
+// 特性
+public:
+	C实验52（2）Doc* GetDocument() const;
+
+// 操作
+public:
+	CBitmap m_Bitmap;
+	int m_nWidth;
+	int m_nHeight;
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~C实验52（2）View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // 实验5.2（2）View.cpp 中的调试版本
+inline C实验52（2）Doc* C实验52（2）View::GetDocument() const
+   { return reinterpret_cast<C实验52（2）Doc*>(m_pDocument); }
+#endif
+
