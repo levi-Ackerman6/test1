c53c02be6da5f3e5830d6b7da5f2b0711c5015cd
levi-Ackerman6 <2535133278@qq.com>
2020-07-06 01:11:57 +0800

    221

---
+++ 实验2.2（1）/实验2.2（1）/实验2.2（1）View.cpp
@@ -0,0 +1,87 @@
+
+// 实验2.2（1）View.cpp : C实验22（1）View 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "实验2.2（1）.h"
+#endif
+
+#include "实验2.2（1）Doc.h"
+#include "实验2.2（1）View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// C实验22（1）View
+
+IMPLEMENT_DYNCREATE(C实验22（1）View, CView)
+
+BEGIN_MESSAGE_MAP(C实验22（1）View, CView)
+END_MESSAGE_MAP()
+
+// C实验22（1）View 构造/析构
+
+C实验22（1）View::C实验22（1）View()
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+C实验22（1）View::~C实验22（1）View()
+{
+}
+
+BOOL C实验22（1）View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// C实验22（1）View 绘制
+
+void C实验22（1）View::OnDraw(CDC* pDC)
+{
+	C实验22（1）Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+	CString s=_T("我是***");
+	pDC->TextOutW(200, 200, s);
+	int A = 6;
+	CString ss;
+	ss.Format(_T("%d"), A);
+	pDC->TextOutW(100, 100, ss);
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// C实验22（1）View 诊断
+
+#ifdef _DEBUG
+void C实验22（1）View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void C实验22（1）View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+C实验22（1）Doc* C实验22（1）View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(C实验22（1）Doc)));
+	return (C实验22（1）Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// C实验22（1）View 消息处理程序
